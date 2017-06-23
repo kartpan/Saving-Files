5c872c49a4f3f1216e81913262d99ecd38e9081a
kartpan <karthik.pan@gmail.com>
2017-06-23 17:37:09 +0800

    Saving to shared preferences and files

--- app/src/main/java/com/example/karthik/myapplication/DisplayMessageActivity.java
+++ app/src/main/java/com/example/karthik/myapplication/DisplayMessageActivity.java
@@ -1,12 +1,19 @@
 package com.example.karthik.myapplication;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.StrictMode;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
-
-
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -18,8 +25,43 @@ public class DisplayMessageActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
-        TextView textView = (TextView) findViewById(R.id.textView2);
+        TextView textView = (TextView) findViewById(R.id.interface_message);
         textView.setText(message);
 
     }
+
+    public void loadData(View view) {
+
+        SharedPreferences sharedPref = getSharedPreferences(MainActivity.PREFS_NAME, Context.MODE_PRIVATE);
+        String sharedkey = sharedPref.getString(getString(R.string.key_data),"Unable to read contents");
+        TextView spfile = (TextView) findViewById(R.id.saved_text);
+        spfile.setText("Saved text in shared preference: \n\n"
+                .concat("Key: ")
+                .concat(getString(R.string.key_data))
+                .concat("\nValue: ")
+                .concat(sharedkey));
+
+        StringBuilder filecontents = new StringBuilder();
+
+        try {
+
+            FileInputStream fis = this.openFileInput("settings");
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader bufferedReader = new BufferedReader(isr);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                filecontents.append(line);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            filecontents.append("file not found");
+        }
+
+        TextView isfile = (TextView) findViewById(R.id.saved_file);
+        isfile.setText("Saved text in internal storage: \n\n"
+                .concat(filecontents.toString()));
+
+    }
+
 }
--- app/src/main/java/com/example/karthik/myapplication/MainActivity.java
+++ app/src/main/java/com/example/karthik/myapplication/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.karthik.myapplication;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Environment;
+import android.preference.PreferenceManager;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
-//import static android.provider.AlarmClock.EXTRA_MESSAGE;
+import java.io.File;
+import java.io.FileOutputStream;
+
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "";
+    public static final String PREFS_NAME = "MySettings";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +35,59 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
 
+    }
+
+
+    public void saveData(View view) {
+
+
+        // To store contents to the shared preferences
+
+        SharedPreferences sharedPref = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        editor.putString(getString(R.string.key_data), message);
+        editor.commit();
+
+        // To store contents to the internal storage
+
+        final String filename = "settings";
+        FileOutputStream outputStream;
+
+        try {
+            outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+            outputStream.write(message.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+        View parentLayout = findViewById(R.id.root_view);
+        final Context context = this;
+
+        Snackbar.make(parentLayout, "Saving Completed", Snackbar.LENGTH_LONG)
+                .setAction("UNDO", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        // Clear shared preferences
+                        SharedPreferences sharedPref = getSharedPreferences(PREFS_NAME,Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPref.edit();
+                        editor.clear();
+                        editor.commit();
+
+                        // Delete file
+                        context.deleteFile("settings");
+
+                        Toast.makeText(context,"Cleared data ...",Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .show();
 
     }
+
 
 }
